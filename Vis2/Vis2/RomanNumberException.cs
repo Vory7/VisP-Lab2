1f64a493c2a3a1656a66684c7dcc2578937bd91e
Vory7 <timofeydorogin1810@gmail.com>
2022-05-05 22:19:17 +0700

    Add programm

---
+++ Vis2/Vis2/RomanNumberException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vis2
+{
+    public class RomanNumberException : Exception
+    {
+        public short Value { get; }
+        public RomanNumberException(string message) : base(message)
+        {
+
+        }
+    }
+}
